00e048225a5fbe544f0ea5dfe6418e9788f5dff3
Мария Одринская <maria6119@yandex.com>
2021-12-10 11:31:14 +0300

    s

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -120,7 +120,7 @@ def add_comment(request, post_id):
 
 @login_required
 def follow_index(request):
-    post_list = Post.objects.filter(author__following__user=request.user)
+    post_list = Post.objects.filter(author__following__user=request.user).all()
     paginator = Paginator(post_list, POSTS_PER_PAGE)
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
