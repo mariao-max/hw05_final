bacf8b2a2f506c1ed6c81fc46191136aa5a2b282
Мария Одринская <maria6119@yandex.com>
2021-12-10 12:54:53 +0300

    done

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -132,7 +132,9 @@ def follow_index(request):
 @login_required
 def profile_follow(request, username):
     author = get_object_or_404(User, username=username)
-    if author != request.user:
+    if (author != request.user
+        and not Follow.objects.filter(user=request.user,
+                                      author=author).exists()):
         Follow.objects.create(user=request.user, author=author)
     return redirect('posts:profile', username=username)
 
